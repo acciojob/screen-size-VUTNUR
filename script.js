fd6c697f62e98d7ea7c258e445e2975143ff4222
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-23 19:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let body=document.getElementsByTagName("body");
+let div=document.createElement("div");
+div.id="sizeInfo"
+let h1=document.createElement("h1");
+h1.innerText="Width: and Height: "
+div.append(h1);
+body.append(div);
