b8e545171162653adb16075c9b27d461dc6e7502
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-06-01 13:03:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //your JS code here. If required.
-
-function updateSize() {
-	const sizeInfo=document.getElementById("sizeInfo");
-	const w= window.innerWidth;
-	const h=window.innerHeight;
-	sizeInfo.innerHTML=`<h1>Width:${w} and Height:${h}</h1>`
-// document.getElementById('sizeInfo').innerHTML="<h1>Width:"+w+" and Height:"+h+"</h1>";
-}
+function getSize() {
+			var w = document.documentElement.clientWidth;
+			var h = document.documentElement.clientHeight;
+			document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+		}
