2ec28e1a2eaf0223b1b45592973dd82d658fdec3
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-06-01 12:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 //your JS code here. If required.
-let body=document.getElementsByTagName("body")[0];
-let div=document.createElement("div");
-div.id="sizeInfo"
-let h1=document.createElement("h1");
-window.addEventListener('load',updateSize())
-window.addEventListener('resize',updateSize())
+
 function updateSize() {
-	h1.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`
-}
-div.append(h1);
-body.append(div);
+	var w= document.documentElement.clientWidth;
+	var h=document.documentElement.clientHeight;
+document.getElementById('sizeInfo').innerHTML="<h1>Width:"+w+" and Height:"+h+"</h1>";}
